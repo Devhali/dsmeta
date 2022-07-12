1519e9abdc3ea8ddf3d76e04efc5d6837463a935
Halisson Rabelo <halissonrabelo@gmail.com>
2022-07-12 16:22:49 -0300

    Project created

---
+++ backend/src/test/java/com/hasvariedades/dsmeta/DsmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hasvariedades.dsmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
