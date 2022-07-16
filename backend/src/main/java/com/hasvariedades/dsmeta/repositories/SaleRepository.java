a9b70e9863ffb76518c5089f4af4b8d718101b96
Halisson Rabelo <halissonrabelo@gmail.com>
2022-07-16 17:24:00 -0300

    API test

---
+++ backend/src/main/java/com/hasvariedades/dsmeta/repositories/SaleRepository.java
@@ -0,0 +1,10 @@
+package com.hasvariedades.dsmeta.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.hasvariedades.dsmeta.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long> {
+
+}
+
